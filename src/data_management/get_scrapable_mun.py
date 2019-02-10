7c4644422af222419d518e9f01c289fec600929f
Maximilian Mähr <maximilian.maehr@gmail.com>
2019-02-10 16:04:19 +0100

    Slight formatting adjustments.

--- src/data_management/get_scrapable_mun.py
+++ src/data_management/get_scrapable_mun.py
@@ -6,14 +6,17 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 from bld.project_paths import project_paths_join as ppj
 
+
 def load_webdriver(url, sec):
     """This functions loads the firefox webdriver for a prespecified 
     url. The sec argument generates some delay to ensure that the 
-    driver is properly loaded before further code is run.""" 
-    driver = webdriver.Firefox(executable_path=r"C:/Users/maxim/Documents/master_eco/eco/geckodriver.exe")
+    driver is properly loaded before further code is run."""
+    driver = webdriver.Firefox(
+        executable_path=r"C:/Users/maxim/Documents/master_eco/eco/geckodriver.exe")
     driver.get(url)
-    # Use some delay, since webdriver needs some time to load. 
+    # Use some delay, since webdriver needs some time to load.
     time.sleep(sec)
+
 
 def get_mun_soup(driver):
     """Given a webdriver object this function returns the
@@ -21,7 +24,7 @@ def get_mun_soup(driver):
     listed on current site."""
     page = driver.execute_script('return document.body.innerHTML')
     soup = BeautifulSoup(''.join(page), 'html.parser')
-    mun_soup = soup.find_all("tr", {"role":"row"})[1:]
+    mun_soup = soup.find_all("tr", {"role": "row"})[1:]
     return mun_soup
 
 
@@ -33,9 +36,10 @@ def fill_mun_dict(mun, mun_df):
     mun_dict["url"] = mun.a["url"]
     mun_dict["municipality"] = mun.a.text
     mun_dict["state"] = mun.find_all("td")[2].text
-                
+
     mun_df = mun_df.append(votes_dict, ignore_index=True)
     return mun_df
+
 
 def run_scraping(driver):
     """This function scrapes all relevant information of municipalities
@@ -44,7 +48,7 @@ def run_scraping(driver):
     the votemanager site is reached."""
     mun_df = pd.DataFrame()
 
-    while True:    
+    while True:
         mun_soup = get_mun_soup(driver)
 
         # Loop through all municipalities on a site.
@@ -54,22 +58,23 @@ def run_scraping(driver):
 
         # Go to next page when all municipalties are scraped.
         driver.find_element_by_xpath('//a[text()="weiter"]').click()
-        
+
         # After each iteration check if last page is reached.
         if soup.find("li", {"class": "paginate_button next disabled"}) != None:
             driver.close()
             break
 
     # Idnetiefy scrapable municipalties by substing in url.
-    key_indicators = ["://votemanager.", "://wahlen."]    
-    mun_df["scrapable"] = mun_df["href"].apply(lambda x: 1 if any(key in x for key in key_indicators) else 0)
+    key_indicators = ["://votemanager.", "://wahlen."]
+    mun_df["scrapable"] = mun_df["href"].apply(
+        lambda x: 1 if any(key in x for key in key_indicators) else 0)
     return mun_df
 
 if __name__ == '__main__':
     # Needed to work with htpps sites.
     http = urllib3.PoolManager(
-            cert_reqs='CERT_REQUIRED',
-            ca_certs=certifi.where())
+        cert_reqs='CERT_REQUIRED',
+        ca_certs=certifi.where())
 
     # Open Firefox driver and open votemanager site.
     votemanger_url = "http://wahlen.votemanager.de"
