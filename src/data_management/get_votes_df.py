0bf48ebbc90de13ee46353f6fb5bce8c392a1033
Maximilian Mähr <maximilian.maehr@gmail.com>
2019-02-11 17:29:28 +0100

    Adjusted to wotk with dictionary throughout the scraping process that after each iteration is appended to the final dataframe. Also made efficiency changes in the election check part.

--- src/data_management/get_votes_df.py
+++ src/data_management/get_votes_df.py
@@ -6,168 +6,148 @@ from tqdm import tqdm
 from bld.project_paths import project_paths_join as ppj
 
 
-def get_elections_soup(municipality_url):
-    municipality_page = http.request("GET", municipality_url)
-    page_soup = BeautifulSoup(municipality_page.data, 'lxml')
+def get_soup_object(url):
+    """This function returns a soup object from a given url."""
+    http = urllib3.PoolManager(
+        cert_reqs='CERT_REQUIRED',
+        ca_certs=certifi.where()
+    )
+
+    page_request = http.request("GET", url)
+    page_soup = BeautifulSoup(page_request.data, 'lxml')
+    return page_soup
+
+
+def get_elections_soup(mun_url):
+    """This function returns a soup object containing all elections
+    contained in the municipality url."""
+    page_soup = get_soup_object(mun_url)
     elections_soup = page_soup.find_all("tr")[1:]
     return elections_soup
 
 
-def get_export_url(level_soup, election_url):
-    export_soup = level_soup.find_all("ul", {"class": "dropdown-menu"})[-1]
-    export_href = export_soup.find_all("a")[-1]["href"]
-    votes_dict["export_url"] = election_url.rsplit(
-        '/', 1)[0] + '/' + export_href
+def get_elec_type_str(elec):
+    """This function returns the name of the considered election."""
+    elec_date = elec.a.text
+    elec_type_str = elec.text.strip().replace(elec_date, "")
+
+    elec_dict["elec_date"] = elec_date
+    return elec_type_str
 
 
-def get_voting_level(poss_election, election_url):
-    zweitstimmen = poss_election.a["href"]
-    zweitstimmen_url = election_url.rsplit('/', 1)[0] + '/' + zweitstimmen
-    zweitstimmen_page = http.request("GET", zweitstimmen_url)
-    zweitstimmen_soup = BeautifulSoup(zweitstimmen_page.data, 'lxml')
+def get_pssbl_elections(elec):
+    """This function returns all elections that took place on a certain date.
+    The need for this function arises from the fact, that more than one election
+    can take place on the same day and that some elections have the possibility
+    of Erst- and Zweistimmen."""
+    elec_url = mun_url.rsplit('/', 2)[0] + elec.a["href"][2:]
+    elec_soup = get_soup_object(elec_url)
 
-    voting_level = zweitstimmen_soup.find_all(
+    elec_dict["elec_url"] = elec_url
+
+    poss_elections = elec_soup.find_all("div", {"class": "well well-sm"})
+    return poss_elections
+
+
+def get_voting_lvl(poss_election):
+    """For each election data are available for different aggregation levels.
+    I am only interested in the lowest level."""
+    sublvl_href = poss_election.a["href"]
+    sublvl_url = elec_dict["elec_url"].rsplit(
+        '/', 1)[0] + '/' + sublvl_href
+    sublvl_soup = get_soup_object(sublvl_url)
+
+    voting_lvl = sublvl_soup.find_all(
         "div", {"class": "container-fluid"})[0]
-    voting_level = voting_level.find("ul", {"class": "nav navbar-nav"})
-    voting_level = voting_level.find_all("a")
-    return zweitstimmen_url, voting_level
+    voting_lvl = voting_lvl.find("ul", {"class": "nav navbar-nav"})
+    voting_lvl = voting_lvl.find_all("a")
+    return voting_lvl
 
 
-def get_poss_elections(municipality_url, election):
-    election_url = municipality_url.rsplit('/', 2)[0] + election.a["href"][2:]
-    election_page = http.request("GET", election_url)
-    election_soup = BeautifulSoup(election_page.data, 'lxml')
-    poss_elections = election_soup.find_all("div", {"class": "well well-sm"})
-    return election_url, poss_elections
-
-
-def get_election_type_str(election):
-    election_date = election.a.text
-    election_type_str = election.text.strip().replace(election_date, "")
-    return election_date, election_type_str
-
-
-def bundestag_check(election_type_str, poss_elections, votes_df, election_url, election_date, municipality_url):
-    if "Bundestag" in election_type_str:
-        for poss_election in poss_elections:
-            if all(word in poss_election.script.get_text() for word in ["Bundestag", "Zweitstimmen"]):
-                zweitstimmen_url, voting_level = get_voting_level(
-                    poss_election, election_url)
-
-                for level in voting_level:
-                    if "Wahlbezirk" in level.text:
-                        votes_dict = dict()
-                        votes_dict = fill_votes_dict(
-                            votes_dict, zweitstimmen_url, level, "BW", municipality_url, election_date, election_url)
-                        votes_df = votes_df.append(
-                            votes_dict, ignore_index=True)
-    return votes_df
-
-
-def landtag_check(election_type_str, poss_elections, votes_df, election_url, election_date, municipality_url):
-    if "Landtag" in election_type_str:
-        for poss_election in poss_elections:
-            if all(word in poss_election.script.get_text() for word in ["Landtag", "Zweitstimmen"]):
-                zweitstimmen_url, voting_level = get_voting_level(
-                    poss_election, election_url)
-
-                for level in voting_level:
-                    if "Stimmbezirk" in level.text:
-                        votes_dict = dict()
-                        votes_dict = fill_votes_dict(
-                            votes_dict, zweitstimmen_url, level, "LW", municipality_url, election_date, election_url)
-                        votes_df = votes_df.append(
-                            votes_dict, ignore_index=True)
-    return votes_df
-
-
-def europawahl_check(election_type_str, poss_elections, votes_df, election_url, election_date, municipality_url):
-    if "Europa" in election_type_str:
-        for poss_election in poss_elections:
-            if "Europa" in poss_election.script.get_text():
-                zweitstimmen_url, voting_level = get_voting_level(
-                    poss_election, election_url)
-
-                for level in voting_level:
-                    if "Wahlbezirk" in level.text:
-                        votes_dict = dict()
-                        votes_dict = fill_votes_dict(
-                            votes_dict, zweitstimmen_url, level, "EW", municipality_url, election_date, election_url)
-                        votes_df = votes_df.append(
-                            votes_dict, ignore_index=True)
-    return votes_df
-
-
-def fill_votes_dict(votes_dict, zweitstimmen_url, level, election_abbrev, municipality_url, election_date, election_url):
-    votes_dict["municipality_url"] = municipality_url
-    votes_dict["election_date"] = election_date
-    votes_dict["election_type"] = election_abbrev
-    votes_dict["election_url"] = election_url
-    votes_dict["zweitstimmen_url"] = zweitstimmen_url
-    votes_dict["voting_level"] = level.text
-
+def get_dwnld_url(level, elec_abbrev):
+    """This function returns the download url for a given voting level."""
     level_href = level["href"]
     if "Erststimmen" in level_href:
         level_href = level_href.replace(
             "Erststimmen", "Zweitstimmen")
 
-    level_url = election_url.rsplit(
+    level_url = elec_dict["elec_url"].rsplit(
         '/', 1)[0] + '/' + level_href
-    level_page = http.request("GET", level_url)
-    level_soup = BeautifulSoup(
-        level_page.data, 'lxml')
+    level_soup = get_soup_object(level_url)
 
-    votes_dict["level_url"] = level_url
-    votes_dict["export_url"] = get_export_url(
-        level_soup, election_url)
+    dwnld_soup = level_soup.find_all("ul", {"class": "dropdown-menu"})[-1]
+    dwnld_href = dwnld_soup.find_all("a")[-1]["href"]
+    dwnld_url = elec_dict["elec_url"].rsplit(
+        '/', 1)[0] + '/' + dwnld_href
 
-    return votes_dict
+    elec_dict["dwnld_url"] = dwnld_url
+    elec_dict["abbrev"] = elec_abbrev
+
+    return elec_dict
 
 
-def run_scrapping(municipality_url, votes_df):
-    elections_soup = get_elections_soup(municipality_url)
+def elec_type_check(elec_type_str, pssbl_elections, elec_type, elec_df):
+    """This function checks, wheter a possible elections fulfills the requirements
+    of the elections we want to download. The specific requirements are stored in 
+    the elec_type dictionary."""
+    if elec_type["srch_trm"] in elec_type_str:
+        for poss_elec in pssbl_elections:
+            if all(word in poss_elec.script.get_text() for word in [elec_type["srch_trm"], elec_type["class"]]):
+                voting_lvl = get_voting_lvl(poss_elec)
 
-    for election in elections_soup:
-        election_date, election_type_str = get_election_type_str(election)
+                for level in voting_lvl:
+                    if elec_type["level"] in level.text:
+                        elec_dict = get_dwnld_url(level, elec_type["abbrev"])
+                        elec_df = elec_df.append(elec_dict, ignore_index=True)
+    return elec_df
 
-        if any(word in election_type_str for word in ["Bundestag", "Landtag", "Europa"]):
-            election_url, poss_elections = get_poss_elections(
-                municipality_url, election)
 
-            votes_df = bundestag_check(
-                election_type_str, poss_elections, votes_df, election_url, election_date, municipality_url)
-            votes_df = landtag_check(
-                election_type_str, poss_elections, votes_df, election_url, election_date, municipality_url)
-            votes_df = europawahl_check(
-                election_type_str, poss_elections, votes_df, election_url, election_date, municipality_url)
-        else:
-            pass
-    return votes_df
+def run_scrapping(mun_url, elec_df, elec_type_dict):
+    """Run web scraping process over all scrapable municipalities. In the end 
+    a dataframe containing all download urls is returned for those elections
+    of interest."""
+    elec_soup = get_elections_soup(mun_url)
+
+    for elec in elec_soup:
+        elec_type_str = get_elec_type_str(elec)
+        elections_of_interest = ["Bundestag", "Landtag", "Europa"]
+        if any(word in elec_type_str for word in elections_of_interest):
+            pssbl_elections = get_pssbl_elections(elec)
+
+            for elec_type in elec_type_dict.values():
+                elec_df = elec_type_check(
+                    elec_type_str, pssbl_elections, elec_type, elec_df)
+
+    return elec_df
 
 if __name__ == '__main__':
-    http = urllib3.PoolManager(
-        cert_reqs='CERT_REQUIRED',
-        ca_certs=certifi.where())
-
     # Read in municipality_df.
-    municipality_df = pd.read_csv(
-        ppj("OUT_DATA", "mun_df.csv"), encoding='cp1252')
+    elec_mun_df = pd.read_csv(
+        ppj("OUT_DATA", "election_mun.csv"), encoding='cp1252')
 
-    # Run web scraping device only on scrappable municipalities as defined in
-    # get_mun.py
-    scrappable_municipalities = municipality_df[
-        municipality_df["scrappable"] == 1]["href"].tolist()
-    # Just for faster computation in this phase
-    scrappable_municipalities = scrappable_municipalities[0:10]
+    scrapable_mun = elec_mun_df[elec_mun_df["scrapable"] == 1]["href"].tolist()[
+        0:10]
+
+    bw_dict = {"srch_trm": "Bundestag", "class": "Zweitstimmen",
+               "level": "Wahlbezirk", "abbrev": "BW"}
+    lw_dict = {"srch_trm": "Landtag", "class": "Zweitstimmen",
+               "level": "Stimmbezirk", "abbrev": "LW"}
+    ew_dict = {"srch_trm": "Europa", "class": "Europa",
+               "level": "Wahlbezirk", "abbrev": "EW"}
+
+    elec_type_dict = {"Bundestagswahl": bw_dict,
+                      "Landtagswahl": lw_dict,
+                      "Europawahl": ew_dict}
 
     # Start scraping process.
-    votes_df = pd.DataFrame()
-    votes_dict = dict()
+    elec_df = pd.DataFrame()
+    elec_dict = {}
 
-    for municipality_url in tqdm(scrappable_municipalities):
+    for mun_url in tqdm(scrapable_mun):
         try:
-            votes_df = run_scrapping(municipality_url, votes_df)
+            elec_df = run_scrapping(mun_url, elec_df, elec_type_dict)
         except:
             pass
 
-    votes_df.to_csv(ppj("OUT_DATA", "votes_df.csv"))
+    # Save election dataframe to .csv.
+    elec_df.to_csv(ppj("OUT_DATA", "election_urls.csv"))
