bc091b9c9279e855376708d4f43fced85fb57b15
Maximilian Mähr <maximilian.maehr@gmail.com>
2019-02-11 18:59:42 +0100

    Ajusted format, changed some variable names and added docstrings.

--- src/analysis/longlat_elec_office.py
+++ src/analysis/longlat_elec_office.py
@@ -2,33 +2,44 @@ import geocoder
 import pandas as pd
 from bld.project_paths import project_paths_join as ppj
 
-elec_comb_df = pd.read_csv(
-    ppj("OUT_DATA", "election_combined.csv"), encoding='cp1252')
 
-exclusion_dict = {"Briefwahl": None, "Wahlbezirk": None,
-                  "Stimmbezirk": None, "Briefwahlbezirk": None, "Wahlbez": None}
+def get_srch_term_list(elec_comb_df):
+    exclusion_dict = {"Briefwahl": None, "Wahlbezirk": None,
+                      "Stimmbezirk": None, "Briefwahlbezirk": None, "Wahlbez": None}
 
-combined_votes["elec_off_srch_term"] = combined_votes[
-    "mun_name"] + " " + combined_votes["Name"]
-combined_votes["elec_off_srch_term"].replace(
-    exclusion_dict, regex=True, inplace=True)
-elec_offices = combined_votes["elec_off_srch_term"].unique().tolist()[0:10]
-elec_offices = [x for x in elec_offices if x is not None]
+    elec_comb_df["srch_term"] = elec_comb_df[
+        "mun_name"] + " " + elec_comb_df["Name"]
+    elec_comb_df["srch_term"].replace(
+        exclusion_dict, regex=True, inplace=True)
+    srch_term_list = elec_comb_df["srch_term"].unique().tolist()[0:10]
+    srch_term_list = [x for x in srch_term_list if x is not None]
+    return srch_term_list
 
-elec_off_df = pd.DataFrame()
-elec_off_dict = {}
 
-# Do the google analysis
-key = "AIzaSyCfFTBllwpO1fIkKbUvduBDyo_WXXxAFZE"
+if __name__ == '__main__':
+    # Read in combined election csv.
+    elec_comb_df = pd.read_csv(
+        ppj("OUT_DATA", "election_combined.csv"), encoding='cp1252')
 
-for office in elec_offices:
-    geo = geocoder.google(office, key=key)
-    elec_off_dict["elec_off"] = elec_office
-    elec_off_dict["lat"] = geo.latlng[0]
-    elec_off_dict["long"] = geo.latlng[1]
-    elec_off_df = elec_off_df.append(
-        elec_off_dict, ignore_index=True)
+    # Initialize pandas dataframe and dictionary to store coordinates.
+    elec_off_df = pd.DataFrame()
+    elec_off_dict = {}
 
-elec_final_df = pd.merge(elec_comb_df, elec_off_df,  how='left', left_on=[
-                          'elec_off_srch_term'], right_on=['elec_off'])
-elec_final_df.to_csv(ppj("OUT_DATA", "election_final.csv"))
+    # Combine election office name plus municipality name to get list of
+    # search terms to be used in google search.
+    srch_term_list = get_srch_term_list(elec_comb_df)
+
+    # API-Key for google analysis.
+    key = "AIzaSyCfFTBllwpO1fIkKbUvduBDyo_WXXxAFZE"
+    # Get coordinates for search term.
+    for srch_term in srch_term_list:
+        geo = geocoder.google(srch_term, key=key)
+        elec_off_dict["srch_term"] = srch_term
+        elec_off_dict["lat"] = geo.latlng[0]
+        elec_off_dict["long"] = geo.latlng[1]
+        elec_off_df = elec_off_df.append(elec_off_dict, ignore_index=True)
+
+    # Merge latitude and longitude data to combined election dataframe.
+    elec_final_df = pd.merge(elec_comb_df, elec_off_df,  how='left', left_on=[
+                             'elec_off_srch_term'], right_on=['srch_term'])
+    elec_final_df.to_csv(ppj("OUT_DATA", "election_final.csv"))
