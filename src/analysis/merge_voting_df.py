cd34526f2f39885f5a8ccac2318a3c93bd885f36
Maximilian Mähr <maximilian.maehr@gmail.com>
2019-02-11 19:23:03 +0100

    Adjusted output folders.

--- src/analysis/merge_voting_df.py
+++ src/analysis/merge_voting_df.py
@@ -1,26 +1,25 @@
 import pandas as pd
-
+from bld.project_paths import project_paths_join as ppj
 
 if __name__ == '__main__':
-    municipality_df = pd.read_csv(
-        ppj("OUT_DATA", "mun_df.csv"), encoding='cp1252')
+    elec_mun_df = pd.read_csv(
+        ppj("OUT_DATA_ELEC", "election_mun.csv"), encoding='cp1252')
 
-    votes_df = pd.read_csv(
-        ppj("OUT_DATA", "votes_df.csv"), encoding='cp1252')
+    elec_df = pd.read_csv(
+        ppj("OUT_DATA_ELEC", "election_urls.csv"), encoding='cp1252')
 
-
-    votes_df = pd.merge(votes_df, municipality_df,  how='left',
-                        left_on=['municipality_url'], right_on=['href'])
+    elec_master_df = pd.merge(elec_df, elec_mun_df,  how='left',
+                        left_on=['mun_url'], right_on=['href'])
 
     umlaute_dict = {"ä": "ae", "Ä": "Ae", "ö": "oe",
                     "Ö": "Oe", "ü": "ue", "Ü": "Ue", "ß": "ss"}
 
-    votes_df.replace(umlaute_dict, regex=True, inplace=True)
+    elec_master_df = elec_master_df.replace(umlaute_dict, regex=True, inplace=True)
 
-    votes_df["mun_clearname"] = votes_df["municipality"].apply(
+    elec_master_df["mun_clearname"]=elec_master_df["mun"].apply(
         lambda x: re.sub('Stadt |Gemeinde ', '', x))
 
-    votes_df["ID"] = votes_df["mun_clearname"] + "_" + \
-        votes_df["election_type"] + "_" + votes_df["election_date"]
+    elec_master_df["ID"]=elec_master_df["mun_clearname"] + "_" +
+        votes_df["elec_type"] + "_" + votes_df["elec_date"]
 
-    votes_df.to_csv(ppj("OUT_DATA", "combined_votes_df.csv"))
+    elec_master_df.to_csv(ppj("OUT_DATA_ELEC", "election_master.csv"))
