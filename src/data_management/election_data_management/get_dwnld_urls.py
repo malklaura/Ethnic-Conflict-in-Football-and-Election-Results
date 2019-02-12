a11f039cb14a90ca2aa0f91d20018777a9018d4b
Maximilian Mähr <maximilian.maehr@gmail.com>
2019-02-12 12:29:18 +0100

    Added .py files to new factor structure.

---
+++ src/data_management/election_data_management/get_dwnld_urls.py
@@ -0,0 +1,154 @@
+import certifi
+import urllib3
+import pandas as pd
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+from bld.project_paths import project_paths_join as ppj
+
+
+def get_soup_object(url):
+    """This function returns a soup object from a given url."""
+    http = urllib3.PoolManager(
+        cert_reqs='CERT_REQUIRED',
+        ca_certs=certifi.where()
+    )
+
+    page_request = http.request("GET", url)
+    page_soup = BeautifulSoup(page_request.data, 'lxml')
+    return page_soup
+
+
+def get_elections_soup(mun_url):
+    """This function returns a soup object containing all elections
+    contained in the municipality url."""
+    page_soup = get_soup_object(mun_url)
+    elections_soup = page_soup.find_all("tr")[1:]
+    return elections_soup
+
+
+def get_elec_type_str(elec):
+    """This function returns the name of the considered election."""
+    elec_date = elec.a.text
+    elec_type_str = elec.text.strip().replace(elec_date, "")
+
+    elec_dict["elec_date"] = elec_date
+    return elec_type_str
+
+
+def get_pssbl_elections(elec):
+    """This function returns all elections that took place on a certain date.
+    The need for this function arises from the fact, that more than one election
+    can take place on the same day and that some elections have the possibility
+    of Erst- and Zweistimmen."""
+    elec_url = mun_url.rsplit('/', 2)[0] + elec.a["href"][2:]
+    elec_soup = get_soup_object(elec_url)
+
+    elec_dict["elec_url"] = elec_url
+
+    poss_elections = elec_soup.find_all("div", {"class": "well well-sm"})
+    return poss_elections
+
+
+def get_voting_lvl(poss_election):
+    """For each election data are available for different aggregation levels.
+    I am only interested in the lowest level."""
+    sublvl_href = poss_election.a["href"]
+    sublvl_url = elec_dict["elec_url"].rsplit(
+        '/', 1)[0] + '/' + sublvl_href
+    sublvl_soup = get_soup_object(sublvl_url)
+
+    voting_lvl = sublvl_soup.find_all(
+        "div", {"class": "container-fluid"})[0]
+    voting_lvl = voting_lvl.find("ul", {"class": "nav navbar-nav"})
+    voting_lvl = voting_lvl.find_all("a")
+    return voting_lvl
+
+
+def get_dwnld_url(level, elec_abbrev):
+    """This function returns the download url for a given voting level."""
+    level_href = level["href"]
+    if "Erststimmen" in level_href:
+        level_href = level_href.replace(
+            "Erststimmen", "Zweitstimmen")
+
+    level_url = elec_dict["elec_url"].rsplit(
+        '/', 1)[0] + '/' + level_href
+    level_soup = get_soup_object(level_url)
+
+    dwnld_soup = level_soup.find_all("ul", {"class": "dropdown-menu"})[-1]
+    dwnld_href = dwnld_soup.find_all("a")[-1]["href"]
+    dwnld_url = elec_dict["elec_url"].rsplit(
+        '/', 1)[0] + '/' + dwnld_href
+
+    elec_dict["dwnld_url"] = dwnld_url
+    elec_dict["abbrev"] = elec_abbrev
+
+    return elec_dict
+
+
+def elec_type_check(elec_type_str, pssbl_elections, elec_type, elec_df):
+    """This function checks, wheter a possible elections fulfills the requirements
+    of the elections we want to download. The specific requirements are stored in 
+    the elec_type dictionary."""
+    if elec_type["srch_trm"] in elec_type_str:
+        for poss_elec in pssbl_elections:
+            if all(word in poss_elec.script.get_text() for word in [elec_type["srch_trm"], elec_type["class"]]):
+                voting_lvl = get_voting_lvl(poss_elec)
+
+                for level in voting_lvl:
+                    if elec_type["level"] in level.text:
+                        elec_dict = get_dwnld_url(level, elec_type["abbrev"])
+                        elec_df = elec_df.append(elec_dict, ignore_index=True)
+    return elec_df
+
+
+def run_scrapping(mun_url, elec_df, elec_type_dict):
+    """Run web scraping process over all scrapable municipalities. In the end 
+    a dataframe containing all download urls is returned for those elections
+    of interest."""
+    elec_dict["mun_url"] = mun_url
+    elec_soup = get_elections_soup(mun_url)
+
+    for elec in elec_soup:
+        elec_type_str = get_elec_type_str(elec)
+        elections_of_interest = ["Bundestag", "Landtag", "Europa"]
+        if any(word in elec_type_str for word in elections_of_interest):
+            pssbl_elections = get_pssbl_elections(elec)
+
+            for elec_type in elec_type_dict.values():
+                elec_df = elec_type_check(
+                    elec_type_str, pssbl_elections, elec_type, elec_df)
+
+    return elec_df
+
+if __name__ == '__main__':
+    # Read in municipality_df.
+    elec_mun_df = pd.read_csv(
+        ppj("OUT_DATA_ELEC", "election_mun.csv"), encoding='cp1252')
+
+    scrapable_mun = elec_mun_df[elec_mun_df["scrapable"] == 1]["elec_page"].tolist()[
+        0:10]
+
+    bw_dict = {"srch_trm": "Bundestag", "class": "Zweitstimmen",
+               "level": "Wahlbezirk", "abbrev": "BW"}
+    lw_dict = {"srch_trm": "Landtag", "class": "Zweitstimmen",
+               "level": "Stimmbezirk", "abbrev": "LW"}
+    ew_dict = {"srch_trm": "Europa", "class": "Europa",
+               "level": "Wahlbezirk", "abbrev": "EW"}
+
+    elec_type_dict = {"Bundestagswahl": bw_dict,
+                      "Landtagswahl": lw_dict,
+                      "Europawahl": ew_dict}
+
+    # Start scraping process.
+    elec_df = pd.DataFrame()
+    elec_dict = {}
+
+    for mun_url in tqdm(scrapable_mun):
+        try:
+            elec_df = run_scrapping(mun_url, elec_df, elec_type_dict)
+        except:
+            pass
+
+    # Save election dataframe to .csv.
+    elec_df.to_csv(ppj("OUT_DATA_ELEC", "election_urls.csv"))
---
+++ src/data_management/election_data_management/get_scrapable_mun.py
@@ -0,0 +1,86 @@
+import urllib3
+import certifi
+import time
+import pandas as pd
+from selenium import webdriver
+from bs4 import BeautifulSoup
+from bld.project_paths import project_paths_join as ppj
+
+
+def load_webdriver(url, sec):
+    """This functions loads the firefox webdriver for a prespecified 
+    url. The sec argument generates some delay to ensure that the 
+    driver is properly loaded before further code is run."""
+    driver = webdriver.Firefox(
+        executable_path=r"C:/Users/maxim/Documents/master_eco/eco/geckodriver.exe")
+    driver.get(url)
+    # Use some delay, since webdriver needs some time to load.
+    time.sleep(sec)
+    return driver
+
+
+def get_mun_soup(driver):
+    """Given a webdriver object this function returns the
+    corresponding soup object containing all municipalites
+    listed on current site."""
+    page = driver.execute_script('return document.body.innerHTML')
+    soup = BeautifulSoup(''.join(page), 'html.parser')
+    mun_soup = soup.find_all("tr", {"role": "row"})[1:]
+    return soup, mun_soup
+
+
+def fill_mun_dict(mun, elec_mun_df):
+    """This functions stores the name, state and url of a single
+    municipality in a dictionary that afterwards is appended to 
+    a prespecified dataframe."""
+    mun_dict = dict()
+    mun_dict["elec_page"] = mun.a["href"]
+    mun_dict["mun_name"] = mun.a.text
+    mun_dict["state"] = mun.find_all("td")[2].text
+
+    elec_mun_df = elec_mun_df.append(mun_dict, ignore_index=True)
+    return elec_mun_df
+
+
+def run_scraping(driver):
+    """This function scrapes all relevant information of municipalities
+    in the state of NRW. As soon as the end of a page is read, the scraping
+    automatically continues on the next site, until the last page of 
+    the votemanager site is reached."""
+    elec_mun_df = pd.DataFrame()
+
+    while True:
+        soup, mun_soup = get_mun_soup(driver)
+
+        # Loop through all municipalities on a site.
+        for mun in mun_soup:
+            if mun.find_all("td")[2].text == "Nordrhein-Westfalen":
+                elec_mun_df = fill_mun_dict(mun, elec_mun_df)
+
+        # Go to next page when all municipalties are scraped.
+        driver.find_element_by_xpath('//a[text()="weiter"]').click()
+
+        # After each iteration check if last page is reached.
+        if soup.find("li", {"class": "paginate_button next disabled"}) != None:
+            driver.close()
+            break
+
+    # Idnetiefy scrapable municipalties by substing in url.
+    key_indicators = ["://votemanager.", "://wahlen."]
+    elec_mun_df["scrapable"] = elec_mun_df["elec_page"].apply(
+        lambda x: 1 if any(key in x for key in key_indicators) else 0)
+    return elec_mun_df
+
+if __name__ == '__main__':
+    # Needed to work with htpps sites.
+    http = urllib3.PoolManager(
+        cert_reqs='CERT_REQUIRED',
+        ca_certs=certifi.where())
+
+    # Open Firefox driver and open votemanager site.
+    votemanger_url = "http://wahlen.votemanager.de"
+    driver = load_webdriver(votemanger_url, 5)
+
+    # Run scraping process, afterwards store resulting dataframe.
+    elec_mun_df = run_scraping(driver)
+    elec_mun_df.to_csv(ppj("OUT_DATA_ELEC", "election_mun.csv"))
