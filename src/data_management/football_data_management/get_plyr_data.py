88f2055dc6cdfebd163363c90097c9e75b6154b8
Maximilian Mähr <maximilian.maehr@gmail.com>
2019-02-14 09:54:39 +0100

    Adjusted scraping process and commented out scraping of previous stations, which speeds up scraping.

--- src/data_management/football_data_management/get_plyr_data.py
+++ src/data_management/football_data_management/get_plyr_data.py
@@ -1,95 +1,105 @@
-from bs4 import BeautifulSoup
 import certifi
 import csv
 import datetime
 import glob
+import re
+import urllib3
 import numpy as np
 import pandas as pd
-import re 
-import urllib3
+from bs4 import BeautifulSoup
 from bld.project_paths import project_paths_join as ppj
 
 
-df_list = glob.glob('df_**.csv')
+def get_unique_plyrs(game_df):
 
-# First determine the field names from the top line of each input file
-# Comment 1 below
-fieldnames = []
-for filename in df_list:
-  with open(filename, "r", newline="") as f_in:
-    reader = csv.reader(f_in, delimiter = ";")
-    headers = next(reader)
-    for h in headers:
-      if h not in fieldnames:
-        fieldnames.append(h)
+    # Get columns containining individual player urls from homeside and
+    # awayside teams.
+    filter_col = [col for col in game_df if f'{col[0]}{col[-1]}'.lower() == 'hmsd_plyr_url'][1:]
+    filter_col += [col for col in game_df if f'{col[0]}{col[-1]}'.lower() == 'awsd_plyr_url'][1:]
 
-# Then copy the data
-with open("merged_games_df.csv", "w", newline="") as f_out:   # Comment 2 below
-  writer = csv.DictWriter(f_out, fieldnames=fieldnames, delimiter = ";")
-  writer.writeheader()
-  for filename in df_list:
-    with open(filename, "r", newline="") as f_in:
-      reader = csv.DictReader(f_in, delimiter = ";")  # Uses the field names in this file
-      for line in reader:
-        # Comment 3 below
-        writer.writerow(line)
-   
-# Read in newly merged dataset     
-merged_df = pd.read_csv("merged_games_df.csv", sep = ";", encoding='cp1252')
+    # Only keep unique player urls.
+    unique_plyrs = []
+    for col in filter_col:
+        unique_plyrs += game_df[col].unique_plyrs().tolist()
 
-filter_col = [col for col in merged_df if f'{col[0]}{col[-1]}'.lower() == 'hl'][1:]
-filter_col = filter_col + [col for col in merged_df if f'{col[0]}{col[-1]}'.lower() == 'al'][1:]
+    unique_plyrs = list(set(unique_plyrs))[1:]
 
-unique_plyrs = []
-for col in filter_col:
-    unique_plyrs = merged_df[col].unique_plyrs().tolist() + unique_plyrs
-    
-unique_plyrs = list(set(unique_plyrs))[1:]
+    return unique_plyrs
 
-# create players dataset
-http = urllib3.PoolManager(
-        cert_reqs='CERT_REQUIRED',
-        ca_certs=certifi.where())
 
-plyrs_df = pd.DataFrame(columns = ["href", "age", "nat"])
-plyrs_df = pd.DataFrame()
+def get_age_nat_data(plyr_soup, plyr_dict):
+    # Find "td" object containing relevant information.
+    info_soup = plyr_soup.find("td", {"class": "stammdaten"})
 
-for i, url in enumerate(unique_plyrs):
-    plyrs_df.at[i, "href"] = url
-    
-    my_url = url
-    r = http.request("GET", my_url)
-    soup = BeautifulSoup(r.data, 'lxml')
-    
-    info_soup = soup.find("td", {"class":"stammdaten"})
-    
+    # Scrape age and nationality information from each page. Due to the
+    # possible specification of a nickname the location of age and
+    # nationality is not unqiuely defined by a single "tr" object, which 
+    # is why one need to check two possibilities.
     try:
         if "Geburtsdatum" in info_soup.table.find_all("tr")[1].text:
             birthday = info_soup.table.find_all("tr")[1].text
-            plyrs_df.at[i, "age"] = re.search(r'\d{2}.\d{2}.\d{4}', birthday).group(0)
-            plyrs_df.at[i, "nat"] = info_soup.table.find_all("tr")[2].img["title"]
+            plyr_dict["age"] = re.search(
+                r'\d{2}.\d{2}.\d{4}', birthday).group(0)
+            plyr_dict["nat"] = info_soup.table.find_all("tr")[2].img["title"]
         elif "Geburtsdatum" in info_soup.table.find_all("tr")[2].text:
             birthday = info_soup.table.find_all("tr")[2].text
-            plyrs_df.at[i, "age"] = re.search(r'\d{2}.\d{2}.\d{4}', birthday).group(0)
-            plyrs_df.at[i, "nat"] = info_soup.table.find_all("tr")[3].img["title"]
+            plyr_dict["age"] = re.search(
+                r'\d{2}.\d{2}.\d{4}', birthday).group(0)
+            plyr_dict["nat"] = info_soup.table.find_all("tr")[3].img["title"]
         else:
-            plyrs_df.at[i, "age"] = np.nan
-            plyrs_df.at[i, "nat"] = np.nan
-                  
+            plyr_dict["age"] = np.nan
+            plyr_dict["nat"] = np.nan
+    # To ensure that dictionary is appended to dataframe.
     except:
-        plyrs_df.at[i, "age"] = np.nan
-        plyrs_df.at[i, "nat"] = np.nan
-     
-    try:    
-        stations_soup = soup.find("table", {"class": "stationen content_table_std"})
-        stations = stations_soup.find_all("b")
-        saisons = stations_soup.find_all("td", {"class": "saison"})
-        leagues = stations_soup.find_all("div", {"class": "liga_zusatz"})
-        
-        for j, station in enumerate(stations):
-            plyrs_df.at[i, "station_verein_" + str(j)] = station.text
-            plyrs_df.at[i, "saison_verein_" + str(j)] = saisons[j].span.text
-            plyrs_df.at[i, "league_verein_" + str(j)] = leagues[j].a.text
-    
-    except:
-        pass
+        plyr_dict["age"] = np.nan
+        plyr_dict["nat"] = np.nan
+
+    return plyr_dict
+
+
+# def get_stations(plyr_soup, plyr_dict):
+#     try:
+#         stations_soup = plyr_soup.find("table", {"class": "stationen content_table_std"})
+#         stations = stations_soup.find_all("b")
+#         saisons = stations_soup.find_all("td", {"class": "saison"})
+#         leagues = stations_soup.find_all("div", {"class": "liga_zusatz"})
+
+#         for j, station in enumerate(stations[0:10]):
+#             plyr_dict["station_verein_" + str(j)] = station.text
+#             plyr_dict["saison_verein_" + str(j)] = saisons[j].span.text
+#             plyr_dict["league_verein_" + str(j)] = leagues[j].a.text
+
+#     except:
+#         pass
+
+#     return plyr_dict
+
+
+if __name__ == '__main__':
+    # Read in final dataset, containing all games and player data.
+    game_df = pd.read_csv(
+        ppj("OUT_DATA_FOOTBALL", "football_final.csv"), encoding='cp1252')
+
+    unique_plyrs = get_unique_plyrs(game_df)
+
+    # create players dataset
+    http = urllib3.PoolManager(
+        cert_reqs='CERT_REQUIRED',
+        ca_certs=certifi.where())
+
+    plyr_df = pd.DataFrame()
+    plyr_dict = {}
+
+    for url in unique_plyrs:
+        plyr_dict["url"] = url
+
+        page_request = http.request("GET", url)
+        plyr_soup = BeautifulSoup(page_request.data, 'lxml')
+
+        plyr_dict = get_age_nat(plyr_soup, plyr_dict)
+        # Append dictionary to dataframe.
+        plyr_df = plyr_df.append(
+            plyr_dict, ignore_index=True)
+
+    # Save player url, age and nationality in seperate csv file.
+    plyr_df.to_csv(ppj("OUT_DATA_FOOTBALL", "player_data.csv"))
