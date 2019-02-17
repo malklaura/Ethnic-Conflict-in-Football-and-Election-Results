02339f356ef4e9c1e58226db6d7452aa5cd8e7ea
Maximilian Mähr <maximilian.maehr@gmail.com>
2019-02-17 09:13:28 +0100

    Now also postal code and city name are saved from the google search.

--- src/data_management/football_data_management/get_longlat_coords.py
+++ src/data_management/football_data_management/get_longlat_coords.py
@@ -2,35 +2,45 @@ import geocoder
 import pandas as pd
 from bld.project_paths import project_paths_join as ppj
 
+def get_geodata(club_longlat_df, unique_clubs)
+	# Do the google API search by name.
+	api_key = "AIzaSyCfFTBllwpO1fIkKbUvduBDyo_WXXxAFZE"
+
+	for club in unique_clubs[0:10]:
+		club_dict = {}
+		club_dict["hmsd_club"] = club
+		geo = geocoder.google(club, key = api_key)
+		try:
+		    club_dict["hmsd_lat"] = geo.latlng[0]
+		    club_dict["hmsd_long"] = geo.latlng[1]
+		except:
+			pass
+		try:
+		    club_dict["hmsd_postal"] = geo.postal
+		    club_dict["hmsd_locality"] = geo.locality
+		except:
+			pass
+		club_longlat_df = club_longlat_df.append(
+		    club_dict, ignore_index=True)
+
+
 if __name__ == '__main__':
 	# Open final football file csv.
 	final_df = pd.read_csv(ppj("OUT_DATA_FOOTBALL", "football_final.csv"), encoding='cp1252')
 
-	# Get unique set of clubs, consisting of both home and away teams. 
-	unique_clubs = final_df["hmsd_club"].unique().tolist() + final_df["awsd_club"].unique().tolist()
+	# Get unique set of clubs for both home and away teams. 
+	unique_hmsd = final_df["hmsd_club"].unique().tolist()
+	unique_awsd = final_df["awsd_club"].unique().tolist()
+	# Unique intersection.
+	unique_clubs = unique_hmsd + unique_awsd
 	unique_clubs = list(set(unique_clubs))[1:]
 
 	# Create dataframe to store unqiue club - longlat data.
 	club_longlat_df = pd.DataFrame()
 
-	# Do the google API search by name.
-	api_key = "AIzaSyCfFTBllwpO1fIkKbUvduBDyo_WXXxAFZE"
+	club_longlat_df = get_geodata(club_longlat_df, unique_clubs)
 
-	for club in unique_clubs[0:10]:
-		club_dict["club"] = club
-	    geo = geocoder.google(club, key = api_key)
-	    try:
-	        club_dict["lat"] = geo.latlng[0]
-	        club_dict["long"] = geo.latlng[1]
-	    except:
-	        print(club)
+	merged_df = pd.merge(final_df, club_longlat_df,  how='left', on='hmsd_club')
 
-	    club_longlat_df = club_longlat_df.append(
-	        club_dict, ignore_index=True)
-
-	club_longlat_df.to_csv(ppi("OUT_DATA_FOOTBALL", "club_longlat.csv"))
-
-	# merged_df = pd.merge(final_df, clubs_longlat_df,  how='left',
-	#                               left_on=['home_club'], right_on=['club'])
-
-	# merged_df.to_csv(ppj("OUT_DATA_FOOTBALL", "football_final_longlat.csv"))
+	club_longlat_df.to_csv(ppj("OUT_DATA_FOOTBALL", "club_longlat.csv"))
+	merged_df.to_csv(ppj("OUT_DATA_FOOTBALL", "football_final_longlat.csv"))
