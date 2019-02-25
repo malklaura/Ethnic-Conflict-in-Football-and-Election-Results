e4ac73a552d632c2ac9f30d92cdba2ce4eff612a
Maximilian Mähr <maximilian.maehr@gmail.com>
2019-02-25 09:41:55 +0100

    Now running via multiprocessing.

--- src/data_management/election_data_management/get_longlat_coords.py
+++ src/data_management/election_data_management/get_longlat_coords.py
@@ -1,79 +1,103 @@
 import geocoder
 import numpy as np
 import pandas as pd
+import multiprocessing as mp
 from bld.project_paths import project_paths_join as ppj
 
 
-def get_srch_term_list(elec_comb_df):
+def get_srch_term_list(elec_df):
     """This functions returns a list containing all search terms needed for the
     subsequent google search."""
 
+    # Get search term by combining municipal name and election office name.
+    elec_df["srch_term"] = elec_df["mun_clearname"] + \
+        " " + elec_df["elec_off_name"]
+
     # We want to exclude certain words from possible search terms, that could
     # invalidate the google query.
-    exclusion_dict = {"Briefwahl": None, "Wahlbezirk": None,
-                      "Stimmbezirk": None, "Briefwahlbezirk": None, "Wahlbez": None}
+    elec_df["srch_term"] = elec_df["srch_term"].str.replace(r"\(.*\)","")
+    elec_df["srch_term"] = elec_df["srch_term"].str.replace("\d+", "")
+    elec_df["srch_term"] = [clean_srch_term(x) for x in elec_df["srch_term"]]
+    elec_df["srch_term"] = elec_df["srch_term"].str.replace(r"\s\w\s","")
 
-    # Get search term by combining municipal name and election office name.
-    elec_comb_df["srch_term"] = elec_comb_df[
-        "mun_name"] + " " + elec_comb_df["Name"]
-    # Apply exclusion dictionary to this search term column.
-    elec_comb_df["srch_term"].replace(
-        exclusion_dict, regex=True, inplace=True)
     # Only use unique search terms and those which are not None.
-    srch_term_list = elec_comb_df["srch_term"].unique().tolist()
+    srch_term_list = elec_df["srch_term"].unique().tolist()
     srch_term_list = [x for x in srch_term_list if x is not None]
 
-    return srch_term_list[0:10]
+    return srch_term_list
 
 
-def gmaps_elec_offices(srch_term_list, key):
+def clean_srch_term(srch_term):
+    """This functions cleans the search term form unnecessary substrings
+    that could ivalidate the google maps search."""
+
+    exclusion_dict = {"Briefwahl": "",
+                      "Wahlbezirk": "",
+                      "Stimmbezirk": "",
+                      "Briefwahlbezirk": "",
+                      "bezirk": "",
+                      "Obere": "",
+                      "Mittlere": "",
+                      "Untere": ""}
+
+    for word, replacement in exclusion_dict.items():
+        if word in srch_term:
+            return srch_term.replace(word, replacement)
+    return srch_term
+
+
+def gmaps_elec_offices(srch_term):
     """This functions get longitude, latitude and postal code data from a
     google maps search. For each search term a dictionary is initalized
     and appended to a dataframe."""
 
     # Dict and dataframe o store geo information.
-    elec_off_dict = {}
-    elec_off_df = pd.DataFrame()
+    elec_off_dict = {"srch_term": srch_term}
 
     # Get coordinates for search term.
-    for srch_term in srch_term_list:
-        geo = geocoder.google(srch_term, key=key)
+    key = "AIzaSyCfFTBllwpO1fIkKbUvduBDyo_WXXxAFZE"
+    geo = geocoder.google(srch_term, key=key)
 
-        # Store longitude, latitude, postal code and locality in
-        # prespecified dictionary.
-        elec_off_dict["srch_term"] = srch_term
+    # Store longitude, latitude, postal code and locality in
+    # prespecified dictionary.
+    try:
         elec_off_dict["elec_lat"] = geo.latlng[0]
         elec_off_dict["elec_long"] = geo.latlng[1]
+    except:
+        elec_off_dict["elec_lat"] = np.nan
+        elec_off_dict["elec_long"] = np.nan
 
-        # Also try to get postal code and locality name.
-        try:
-            elec_off_dict["elec_postal"] = geo.postal
-            elec_off_dict["elec_locality"] = geo.locality
-        except:
-            elec_off_dict["elec_postal"] = np.nan
-            elec_off_dict["elec_locality"] = np.nan
+    # Also try to get postal code and locality name.
+    try:
+        elec_off_dict["elec_postal"] = geo.postal
+        elec_off_dict["elec_locality"] = geo.locality
+    except:
+        elec_off_dict["elec_postal"] = np.nan
+        elec_off_dict["elec_locality"] = np.nan
 
-        # Append dictionary to dataframe.
-        elec_off_df = elec_off_df.append(elec_off_dict, ignore_index=True)
-
-    return elec_off_df
+    return elec_off_dict
 
 
 if __name__ == '__main__':
     # Read in combined election csv.
-    elec_comb_df = pd.read_csv(
-        ppj("OUT_DATA_ELEC", "election_combined.csv"), encoding='cp1252')
+    elec_df = pd.read_csv(
+        ppj("OUT_DATA_ELEC", "election_combined.csv"))
 
     # Election office name plus municipality name as search name.
-    srch_term_list = get_srch_term_list(elec_comb_df)
-    key = "AIzaSyCfFTBllwpO1fIkKbUvduBDyo_WXXxAFZE"
+    srch_term_list = get_srch_term_list(elec_df)
 
-    long_lat_df = gmaps_elec_offices(srch_term_list, key):
+    # Google maps search via multiprocessing.
+    dict_list = []
+    with mp.Pool() as pool:
+        out = pool.map(gmaps_elec_offices, srch_term_list)
+        dict_list.extend(out)
 
-        # Merge latitude and longitude data to combined election csv.
-    elec_final_df = pd.merge(elec_comb_df, long_lat_df,
+    long_lat_df = pd.DataFrame(dict_list)  # Dicts to dataframe.
+    long_lat_df.to_csv(
+        ppj("OUT_DATA_ELEC", "elec_off_longlat.csv"), index=False)
+
+    # Merge latitude and longitude data to combined election csv.
+    elec_final_df = pd.merge(elec_df, long_lat_df,
                              how='left', on='srch_term')
-
-    # Save as csv file.
     elec_final_df.to_csv(
         ppj("OUT_DATA_ELEC", "election_final.csv"), index=False)
--- src/data_management/football_data_management/get_longlat_coords.py
+++ src/data_management/football_data_management/get_longlat_coords.py
@@ -1,55 +1,57 @@
 import geocoder
 import numpy as np
 import pandas as pd
+import multiprocessing as mp
 from bld.project_paths import project_paths_join as ppj
 
 
-def get_geodata(unique_clubs, key):
+def get_geodata(home_club):
 	"""This function returns longitude and latitude coordinates
 	based on the provided club names. If possible also postal
 	code and location name are returned."""
 
 	# Dict and dataframe o store geo information.
-    club_dict = {}
-    club_df = pd.DataFrame()
-    
-    # Get coordinates for search term.
-    for home_club in unique_clubs:
-        geo = geocoder.google(home_club, key=key)
+	club_dict = {"home_club": home_club}
 
-        # Store longitude, latitude, postal code and locality.
-        club_dict["home_club"] = home_club
-        club_dict["home_lat"] = geo.latlng[0]
-        club_dict["home_long"] = geo.latlng[1]
+	# Get coordinates for search term.
+	key = "AIzaSyCfFTBllwpO1fIkKbUvduBDyo_WXXxAFZE"
+	geo = geocoder.google(home_club, key=key)
 
-        try:
-            club_dict["home_postal"] = geo.postal
-            club_dict["home_locality"] = geo.locality
-        except:
-        	club_dict["home_postal"] = np.nan
-            club_dict["home_locality"] = np.nan
+	# Store longitude, latitude, postal code and locality.
+	try:
+		club_dict["home_lat"] = geo.latlng[0]
+		club_dict["home_long"] = geo.latlng[1]
+	except:
+		club_dict["home_lat"] = np.nan
+		club_dict["home_long"] = np.nan
 
-        # Append dictionary to dataframe.
-        club_df = club_df.append(club_dict, ignore_index=True)
-    
-    return club_df
+	try:
+		club_dict["home_postal"] = geo.postal
+		club_dict["home_locality"] = geo.locality
+	except:
+		club_dict["home_postal"] = np.nan
+		club_dict["home_locality"] = np.nan
+
+	return club_dict
 
 
 if __name__ == '__main__':
 	# Open final football file csv.
-	final_df = pd.read_csv(ppj("OUT_DATA_FOOTBALL", "football_final.csv"), encoding='cp1252')
+	final_df = pd.read_csv(ppj("OUT_DATA_FOOTBALL", "football_combined.csv"))
 
-	# Get unique set of clubs for both home and away teams. 
+	# Get unique set of clubs for both home and away teams.
 	unique_home = final_df["home_club"].unique().tolist()
 	unique_away = final_df["away_club"].unique().tolist()
-	unique_clubs = unique_home + unique_away # Combined list.
-	unique_clubs = list(set(unique_clubs))[1:] # Unique intersection.
+	unique_clubs = unique_home + unique_away  # Combined list.
+	unique_clubs = list(set(unique_clubs))[1:]  # Unique intersection.
 
-	# Do google maps search.
-	api_key = "AIzaSyCfFTBllwpO1fIkKbUvduBDyo_WXXxAFZE"
-	club_longlat_df = get_geodata(unique_clubs, api_key)
+	# Scraping.
+	dict_list = []
+	with mp.Pool() as pool:
+		out = pool.map(get_geodata, unique_clubs)
+		dict_list.extend(out)
+
+	club_longlat_df = pd.DataFrame(dict_list) # Dicts to dataframe.
 
 	# Merge to game data and store as csv.
-	merged_df = pd.merge(final_df, club_longlat_df,  how='left', on='home_club')
 	club_longlat_df.to_csv(ppj("OUT_DATA_FOOTBALL", "club_longlat.csv"), index=False)
-	merged_df.to_csv(ppj("OUT_DATA_FOOTBALL", "football_final_longlat.csv"), index=False)
